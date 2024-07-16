c687b7314a1ecdc4982731faab58ef53e4569f61
nikhilvadithala94 <161732550+nikhilvadithala94@users.noreply.github.com>
2024-07-16 21:28:04 +0530

    fixed resume download issue

--- force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
+++ force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
@@ -1,15 +1,65 @@
-import { LightningElement , api } from 'lwc';
+import { LightningElement , api , wire } from 'lwc';
 import PortfolioObject from '@salesforce/schema/Portfolio__c'
+import getResumeAsAttachment from '@salesforce/apex/PortfolioController.getRelatedFilesByRecordId'
 import ADDRESS from '@salesforce/schema/Portfolio__c.Address__c'
 import PHONE_NUMBER from '@salesforce/schema/Portfolio__c.Mobile_Number__c'
 import EMAIL from '@salesforce/schema/Portfolio__c.Email__c'
+import { NavigationMixin } from 'lightning/navigation'
 
-export default class PortfolioUserDetails extends LightningElement {
+
+export default class PortfolioUserDetails extends  NavigationMixin(LightningElement) {
 
     @api recordId
     @api objectApiName
+    url
 
-    download(){
-
+    @wire(getResumeAsAttachment, {recordId: '$recordId'})
+    wiredResult({data, error}){ 
+        if(data){ 
+            console.log(data)
+            let key = Object.keys(data)[0]
+            this.url = `/sfc/servlet.shepherd/document/download/${key}`
+        }
+        if(error){ 
+            console.log(error)
+        }
     }
+
+
+   // download(){
+       /* getResumeAsAttachment({recordId : this.recordId}).then(
+            result => {
+                this.handleFormatAttachment(result)
+        }).catch(error => {
+            console.log(error)
+            })
+    }
+
+    handleFormatAttachment(res){
+        console.log('entered 2')
+        this.fileList = Object.keys(res).map(item=>({
+            'label' : res[item],
+            'value' : item,
+            'url' :`/sfc/servlet.shepherd/document/download/${item}`
+        }))
+
+            const url = this.fileList[0].url
+           
+            console.log(url)
+            this[NavigationMixin.Navigate]({
+
+                type: standard__webPage,
+
+            attributes: {
+            url: url
+            }
+
+        });
+
+
+    }*/
+
+
+
+
 }
